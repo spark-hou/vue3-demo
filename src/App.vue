a779a449ca33a1117bfe156fc902981aab4c19cc
侯欣榆 <xinyu.hou@dhc.com.cn>
2021-04-14 00:38:46 +0800

    gogog

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,56 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  {{msg}}
+  <div>
+    {{title}}
+    {{userInfo.name}}
+    {{userInfo.age}}
+    {{userCom}}
+    <input type="text" v-model="userInfo.name"/>
+    <input type="text" v-model="name"/>
+  </div>
+  <hr>
+  <Search></Search>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import HelloWorld from './components/HelloWorld.vue'
+  import Search from './components/Search.vue'
+  import {ref, reactive, toRefs, computed} from "vue"
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      HelloWorld, Search
+    },
+    setup() {
+      let title = ref('title')
+      let userInfo = reactive({name: 'one', age: 23})
+      let userCom = computed(() => {
+        return userInfo.name + 'hahah'
+      })
+      return {
+        title, userInfo, ...toRefs(userInfo), userCom
+      }
+
+    },
+    data() {
+      return {
+        msg: 'test'
+      }
+    },
+    methods: {}
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ src/components/Search.vue
@@ -0,0 +1,42 @@
+/**
+* @author  sparkHou
+* @date 2021-04-13 22:28
+* @Description:
+*/
+<template>
+  <div>
+    {{age}}
+  </div>
+</template>
+
+<script>
+  import {reactive, toRefs, watchEffect, watch} from 'vue'
+
+  export default {
+    name: "Search",
+    setup() {
+      let test = reactive({
+        name: 'hhaha',
+        age: 20
+      })
+      watchEffect(() => {
+        console.log(test.age, '====')
+      })
+      watch(test, () => {
+        console.log(test.age, '=watch===')
+
+      })
+      setInterval(() => {
+        test.age++
+      }, 1000)
+      return {
+        ...toRefs(test),
+      }
+
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
